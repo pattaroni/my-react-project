ecac1c41dd2555a4ac9cbb8bb6dc87417b78efd4
Oleksii Kyryk <kirikoleksiy112@gmail.com>
2025-09-13 20:56:43 +0300

    init

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+// src/App.tsx
+import Product from "./Product";
+
+export default function App() {
+  return (
+    <>
+      <h1>Products</h1>
+
+      <Product />
+      <Product />
+      <Product />
+    </>
+  );
+}
